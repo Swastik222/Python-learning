15ca4b593bfea6cb085f5fa12a919dd23b518bb9
Swastik222 <swastikmishra813@gmail.com>
2023-10-14 12:50:17 +0530

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,273 @@
+# ctrl + / for quick comment and dis comment
+
+# vdo 5💻💻💻
+# print("hello swastik",2,2, sep="-" , end="mishra\n")
+# print("hy swastik mishra",2,2, sep="-" , end="mishra\n")
+
+
+# vdo 10💻💻💻
+# a = int(input())
+# b = int(input())
+
+# print(a-b)
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+# vdo 11
+# a= '''hello swastik
+# this is taniya ur
+# friend'''
+# print(a)
+
+# a = " hi swastik this is \"using double quote again technique\" for python called backslash double qoute"
+# print(a)
+
+# vid12💻💻😊
+# fruit="Grapes"
+# fruitlen=len(fruit)
+# print(fruitlen)
+
+# # print(fruit[0:4])
+# # print(fruit[:4])
+
+# print(fruit[-3:-1])
+# print(fruit[-3:len(fruit)-1])
+# OUTPUT HERE IS = PE
+
+# nm="harry"
+# print(nm[-4:-2])
+# output = ar
+
+# VDO 13💻💻
+
+# a="!!!harry!!!"
+# print(a.upper())
+# print(a.lower())
+
+# print(a.strip("!!")) #this is used to not count given symbol /#used to remove staart & ending white spaces
+# print(a.rstrip("!")) #used to remove ending white spaces
+
+# print(a.replace("harry", "swastik"))
+# OUTPUT = !!!HARRY!!!
+#         !!!harry!!!
+#         harry
+#         !!!harry
+#         !!!swastik!!!
+
+# a= "!!!! code with harry !!!"
+# print(a.split())
+# PUT --> ['!!!!', 'code', 'with', 'harry', '!!!']
+
+# a= "code wiTH HaRry"
+# print(a.capitalize())  #out = Code with harry
+# print(len(a))   #out= 15
+# print(len(a.center(50)))    #out= 50
+# print(a.center(50))         #out=                       code with HaRry
+
+# x = "code with harry with harry along harry"
+# print(x.count("harry"))     #out = 3
+
+# str= "!!!! code with harry !!!"
+# print(str.endswith("!!!"))   #out→ true
+
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("to", 4, 10))       #out→ True
+
+# str1 = "He's name is Dan. He is an honest man."
+# print(str1.find("is"))     #out😶‍🌫️   10
+
+
+# vdo 14💻💻💻
+
+# a= int(input("Enter ur Num :"))
+# if(a<0):
+#     print(a , "is negative number")
+# elif(a>0):
+#     if(a<=25): print(a,"is lesser than 25")
+#     elif(a>25 and a<=55):  print(a,"is between 0 to 55")
+#     else: print(a, "is not in range")
+# else: print(a,"is zero")
+
+# vdo 15💻💻  exercise
+
+# import time
+# timestamp = time.strftime('%H:%M:%S:%ms')
+# print(timestamp)
+# timestamp = time.strftime('%H')
+# print(timestamp)
+# timestamp = time.strftime( '%M' )
+# print(timestamp)
+# timestamp = time.strftime("%S")
+# print(timestamp)
+
+# # vdo 16 match case💻
+
+# x=int(input("enter a value \n"))
+
+# match x:
+#     case _ if x!= 20:
+#         print(x,"this is good")
+#     case 20:
+#         print(x,"is very good")
+#     case _:
+#         print(x)
+
+
+# vdo 17 💻💻💻
+
+# for i in range(5):
+#     print(i+1)
+# for x in range(21,30):
+#      print(x)  #out(❁´◡`❁) = 21
+#                         22
+#                         23
+#                         24
+#                         25
+#                         26
+#                         27
+#                         28
+#                         29
+
+#   vdo 18(❁´◡`❁)💻 while loop
+
+# i=0
+# while (i<=13):
+#     print(i)
+#     i=i+1  #out = 1to13
+
+# i=1
+# while(i<=100):
+#     i=int(input("enter the num : "))
+#     print(i," is under 100")
+# print("num is out of range")
+
+# count=5
+# while(count>0):
+#     print(count)
+#     count=count-1
+
+# vdo 19💻(❁´◡`❁)
+
+#   Break statement☆*: .｡. o(≧▽≦)o .｡.:*☆
+# for i in range(10):
+#     if(i==5):
+#         break
+#     print(i)
+
+# Continue statment ☆*: .｡. o(≧▽≦)o .｡.:*☆
+# for i in range(20):
+#     if(i==10):
+#         print ("breaking point")             #10 num pr breaking point print hoga instead of 10 bczz of continue
+#         continue
+#     print(i)
+
+# for i in range(20):
+#     if (i==10):
+#         print("skip the iteration")
+#         continue
+#     print("5 X ",i,"=" ,5*i)
+
+# 👍 Do while loop in python using break
+# 🌟🌟🌟✨ Do while only support in C , C++, java etc not in python thats why we only use💻 while True:
+
+# a=0
+# while True:
+#     print(a)
+#     a=a+1
+#     if(a%100==0):
+#         break      #out👍= 0 to 99 , ^_~ bcz 100 pr break ho jayega 100%100===0
+
+
+# 💻 Vdo 20  🌟`#defining user defined Function in python`
+
+# def gemMean(a,b):
+#     mean= (a*b)/(a+b)
+#     print(mean)
+# def isGreater(a,b):
+#     if(a>b):
+#         print("a is greater than b")
+#     else:
+#         print("b is greater than a")
+# def isLesser(a,b):
+#     pass
+# a=2
+# b=3
+# gemMean(a,b)
+# isGreater(a,b)
+
+# c=10
+# d=20
+# gemMean(c,d)
+# isGreater(c,d)
+
+# 🔳🔳📤Output =
+# 1.2
+# b is greater than a
+# 6.666666666666667
+# b is greater than a
+
+
+# Vdo 21 💻💻 Function argument
+
+# def average(a,b,c=2,d=5):
+#     print("average=",(a+b+c+d)/4)
+# average(3 , 4)
+# average(b=10,a=20)
+
+
+# vdo 22💻🌟 List
+
+# Link to the list 📤⭐     https://www.codewithharry.com/tutorial/python-list-indexes/
+
+# list append:
+# l=[1,22,3,'harry',"swastik",55,10,20]
+# print(l)
+# l.append(11)
+# print(l)
+
+# list sort:
+
+# l=[1,22,3,2,44,55,10,20] 
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l.reverse()
+# print(l)
+# print(l.index(2))
+# print(l.count(2))
+
+l=[1,22,3,2,44,55,10,20]
+m=l.copy() #or m=l[:]
+m[0]=0 #or l[0]=0
+print(l) #or print(m)
+s=[200,88,45]     
+l.extend(s)   #or l.append(s)
+print(l)         #🔳📤out = [0, 22, 3, 2, 44, 55, 10, 20, 200, 88, 45] 
+#or
+k=s+l #or k=l+s
+print(k)        #🔳📤out = [200, 88, 45, 0, 22, 3, 2, 44, 55, 10, 20]
+
+# l=[1,22,3,2,44,55,10,20]
+# print(l[0:3])
+
+# 🌟🌟🌟🌟🌟🌟🌟🌟 Capilot suggestion using  Ctrl + Enter
+
+# 💻vdo 25 Tuple()
+
+# tup=(1,2,3,4,5,1,3,6,3,10)
+# print(tup)
+# print(tup.count(3))       #3 is no. of times 3 is repeated
+# print(tup.index(3))        #3 is at 2nd index
+# print(len(tup))            #length of tuple
+# print(max(tup))            #max no. in tuple
+# print(min(tup))             #min no. in tuple
+# print(sum(tup))            #sum of all no. in tuple
+# print(tup.index(3,4,8))   #3 is no. and 4 is starting index and 8 is ending index
+#                           # 📤🔳#out = 6 (3 is at 6th index)
+# print(tup[0:3])            #📤🔳#out = (1, 2, 3)
+
+                    
