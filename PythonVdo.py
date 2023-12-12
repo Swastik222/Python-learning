dd828fd6a8ddea9c6aa4f7112109a23ad31680ca
Swastik222 <swastikmishra813@gmail.com>
2023-12-12 19:25:53 +0530

    .

--- PythonVdo.py
+++ PythonVdo.py
@@ -554,10 +554,10 @@
 # print(result)  # Output: 3.0
 # print(m.pi)  # Output: 3.141592653589793
 
-from math import sqrt, pi
-result = sqrt(9)
-print(result)  # Output: 3.0
-print(pi)  # Output: 3.141592653589793
+# from math import sqrt, pi
+# result = sqrt(9)
+# print(result)  # Output: 3.0
+# print(pi)  # Output: 3.141592653589793
 
 
 
